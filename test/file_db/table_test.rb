9e42ccc5088e9038c6fae37944644b20cce23928
wRadion <wradion@gmail.com>
2016-11-16 20:12:26 +0100

    Fixed tests

--- test/file_db/table_test.rb
+++ test/file_db/table_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class FileDb::TableTest < Minitest::Test
 
-  before do
+  def setup
     @db = FileDb::Database.new('fixtures/data.json')
     @movies = @db.table('movies')
     @directors = @db.table('directors')
   end
 
   def test_select_with_id
-    assert_equal(@movies.select(where { id: 1 })[:title], 'The Gold Rush')
+    assert_equal(@movies.select(where: { id: 1 })[:title], 'The Gold Rush')
   end
 
   def test_select_with_year
-    assert_equal(@movies.select(where { year: '1936' })[:title], 'Modern Times')
+    assert_equal(@movies.select(where: { year: '1936' })[:title], 'Modern Times')
   end
 
 end
