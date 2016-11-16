0692430136afeaf39565c86c8ec34c9d1de3ce95
wRadion <wradion@gmail.com>
2016-11-16 20:38:09 +0100

    Refactoring

--- lib/file_db/table.rb
+++ lib/file_db/table.rb
@@ -1,33 +1,34 @@
 module FileDb
   class Table
+
     def initialize(rows)
-      @rows = rows
+      @rows = symbolize_hash(rows)
     end
 
     def select(where:)
-      rows_symbolized.select do |row|
-        row.values_at(*where.keys) == where.values
-      end
+      @rows.select { |row| row.values_at(*where.keys) == where.values }
     end
 
-    def insert(object)
-      @rows << object.merge(id: next_id)
+    def insert(row)
+      @rows << row.merge(id: next_id)
     end
 
     private
 
-    def rows_symbolized
-      @rows_symbolized ||= @rows.map do |line|
-        line.inject({}) do |memo,(k,v)|
+    def next_id
+      @max_id ||= @rows.map { |e| e.id }.sort { |a, b| b <=> a }.first
+      @max_id = @max_id + 1
+    end
+
+    def symbolize_hash(rows)
+      rows.map do |line|
+        line.inject({}) do |memo, (k, v)|
+          v = symbolize_hash(v) if v.is_a?(Hash)
           memo[k.to_sym] = v
           memo
         end
       end
     end
 
-    def next_id
-      @max_id ||= @rows.map { |e| e.id }.sort { |a, b| b <=> a }.first
-      @max_id = @max_id + 1
-    end
   end
 end
