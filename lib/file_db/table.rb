8f60a40370ee39b5660a6d9319b57e27a703eb15
wRadion <wradion@gmail.com>
2016-11-16 20:22:32 +0100

    Added Table#insert

--- lib/file_db/table.rb
+++ lib/file_db/table.rb
@@ -1,7 +1,20 @@
 module FileDb
   class Table
+
     def initialize(internal)
       @internal = internal
     end
+
+    def insert(object)
+      @internal << object.merge(id: get_id_and_increment))
+    end
+
+    private
+
+    def get_id_and_increment
+      @max_id ||= @internal.map { |e| e.id }.sort { |a, b| b <=> a }.first
+      @max_id = @max_id + 1
+    end
+
   end
 end
